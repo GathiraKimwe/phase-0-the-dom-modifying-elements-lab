29f11ceb71dfe687653ffc721c029681209b30a8
Edwin Gathira <gathirakimwe@gmail.com>
2022-08-16 22:51:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.body.removeChild(main);
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id','victory');
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
